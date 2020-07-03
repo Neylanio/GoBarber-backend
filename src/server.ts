d884a23c4c5352116264652f2226dee36cd71935
Neylanio <neylaniogomes@gmail.com>
2020-07-03 17:24:56 -0300

    Add Eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express, { request } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/teste', (request, response) => {
-  return response.json({ message: 'Hello Gostack' });
-});
+app.get('/teste', (request, response) => response.json({ message: 'Hello Gostack' }));
 
 app.listen(3333, () => {
-  console.log("🚀 Server started on port 3333")
+  console.log('🚀 Server started on port 3333');
 });
