f024fd40e846feae260f40f03ce9b2dc07823af7
Neylanio <neylaniogomes@gmail.com>
2020-07-03 17:29:35 -0300

    Add Prettier configuration

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/teste', (request, response) => response.json({ message: 'Hello Gostack' }));
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello Gostack' }),
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
