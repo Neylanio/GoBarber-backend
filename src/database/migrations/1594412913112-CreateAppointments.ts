80cdb9ae8cebc30308279812d0096562d9f88412
Neylanio <neylaniogomes@gmail.com>
2020-07-13 20:14:07 -0300

    feat: add relationship between appointments and users
    The table appointments will receive the user id

--- src/database/migrations/1594412913112-CreateAppointments.ts
+++ src/database/migrations/1594412913112-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1594412913112
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
