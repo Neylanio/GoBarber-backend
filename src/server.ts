dfccec64826803a0e41c5e17bf621a2176845647
Neylanio <neylaniogomes@gmail.com>
2020-08-21 15:41:17 -0300

    config: Run on port 3333

--- src/server.ts
+++ src/server.ts
@@ -13,20 +13,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof AppError){
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal server error',
     });
-  }
+  },
+);
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
-
-app.listen(3000, () => {
+app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
 });
