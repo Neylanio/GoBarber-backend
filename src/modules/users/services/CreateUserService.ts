beb64a33febc00c88a679c36c93934dd6a0ca014
Neylanio <neylaniogomes@gmail.com>
2020-09-14 17:42:37 -0300

    feat: Refact modules

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,29 +1,27 @@
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import AppError from '@shared/errors/AppError';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   name: string;
   email: string;
   password: string;
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
-    const checkUserExists = await usersRepository.findOne({
-      where: { email },
-    });
+
+  constructor(private usersRepository: IUsersRepository){}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
 
     if (checkUserExists) throw new AppError('Email address already used');
 
     const newPassword = await hash(password, 8);
 
-    const user = usersRepository.create({ name, email, password: newPassword });
-
-    await usersRepository.save(user);
+    const user = await this.usersRepository.create({ name, email, password: newPassword });
 
     return user;
   }
