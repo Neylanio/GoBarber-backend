fc8c9aea45f563fe3d43df2693057f191048b0ce
Neylanio <neylaniogomes@gmail.com>
2020-09-17 16:59:04 -0300

    feat: Add ExistUser and WrongPassword tests

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -28,4 +28,35 @@ describe('AuthenticateUser', () => {
 
   });
 
+  it('should be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    expect(authenticateUser.execute({
+      email: "neylanio@gmail.com",
+      password: "123456"
+    })).rejects.toBeInstanceOf(AppError);
+
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+    await createUser.execute({
+      email: "neylanio@gmail.com",
+      name: "neylanio",
+      password: "123456"
+    })
+
+    expect(authenticateUser.execute({
+      email: "neylanio@gmail.com",
+      password: "1236"
+    })).rejects.toBeInstanceOf(AppError);
+
+  });
+
 });
