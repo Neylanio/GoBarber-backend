5d613bd7b018ea0fe5e2867965b09683bca00326
Neylanio <neylaniogomes@gmail.com>
2020-09-22 15:39:02 -0300

    test: Add test reset password not able after 2 hours

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -65,4 +65,27 @@ describe('ResetPasswordService', () => {
     ).rejects.toBeInstanceOf(AppError);
   });
 
+  it('should not be able to reset password if passed more than 2 hours', async () => {
+    const user = await fakeUsersRepository.create({
+      email: "ney@gmail.com",
+      name: "neylanio",
+      password: "123456"
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    jest.spyOn(Date, "now").mockImplementationOnce(() => {
+      const customDate = new Date();
+
+      return customDate.setHours(customDate.getHours() + 3);
+    });
+
+    await expect(
+      resetPasswordService.execute({
+      token,
+      password: '123123'
+    })).rejects.toBeInstanceOf(AppError);
+
+  });
+
 });
