7e11c782a46f0ac0b9e789c454fa9bb2e19268c6
Neylanio <neylaniogomes@gmail.com>
2020-09-22 17:11:28 -0300

    feat: Save tokens in database

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,9 +9,11 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRespoitory from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-// import IUserTokensRespoitory from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/';
+import IUserTokensRespoitory from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRespoitory>('AppointmentsRepository', AppointmentsRepository);
 
 container.registerSingleton<IUsersRespoitory>('UsersRepository', UsersRepository);
+
+container.registerSingleton<IUserTokensRespoitory>('UserTokensRepository', UserTokensRepository);
