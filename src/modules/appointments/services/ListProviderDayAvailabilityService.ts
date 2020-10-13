2ecd12f6b2583365347772bf16c370a5316e9e3b
Neylanio <neylaniogomes@gmail.com>
2020-10-13 20:18:28 -0300

    fix: Serialize into cache

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,7 +1,7 @@
-import { injectable, inject } from "tsyringe";
+import { injectable, inject } from 'tsyringe';
 
+import { getHours, isAfter } from 'date-fns';
 import IAppointmentsRespoitory from '../repositories/IAppointmentsRespoitory';
-import { getHours, isAfter } from "date-fns";
 
 interface IRequest {
   provider_id: string;
@@ -10,7 +10,7 @@ interface IRequest {
   day: number;
 }
 
-type IResponse = Array <{
+type IResponse = Array<{
   hour: number;
   available: boolean;
 }>;
@@ -20,32 +20,38 @@ export default class ListProviderDayAvailabilityService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRespoitory: IAppointmentsRespoitory,
-  ){}
+  ) {}
 
-  public async execute({ provider_id, day, month, year }: IRequest): Promise<IResponse> {
-    const appointments = await this.appointmentsRespoitory.findAllInDayFromProvider({ provider_id, day, month, year });
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRespoitory.findAllInDayFromProvider(
+      { provider_id, day, month, year },
+    );
 
     const hourStart = 8;
 
     const eachHourArray = Array.from(
-      {length: 10},
+      { length: 10 },
       (_, index) => index + hourStart,
     );
 
     const currentDate = new Date(Date.now());
 
     const availability = eachHourArray.map(hour => {
-      const hasAppointmentInHour = appointments.find(appointment =>
-        getHours(appointment.date) === hour
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
       );
 
-
-      const compareDate = new Date(year, month -1, day, hour);
+      const compareDate = new Date(year, month - 1, day, hour);
 
       return {
         hour,
         available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
-      }
+      };
     });
 
     return availability;
