a79245576fda96899295c3700ebb03de5a92fd70
Neylanio <neylaniogomes@gmail.com>
2020-10-14 15:00:48 -0300

    refact: Change reset-password address

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -22,33 +22,37 @@ class SendForgotPasswordEmailService {
 
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
-  ){}
+  ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
-
-    if(!user){
-      throw new AppError('User does not exists')
+    if (!user) {
+      throw new AppError('User does not exists');
     }
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    const forgotPasswordTemplate = path.resolve(__dirname, "..", "views", "forgot_password.hbs");
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
-        email: user.email
+        email: user.email,
       },
       subject: '[GoBarber] Recuperação de senha',
       templateData: {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`
-        }
-      }
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
+        },
+      },
     });
   }
 }
