7261808308e1f20efb10ba4172bba8bece8efa1f
Neylanio <neylaniogomes@gmail.com>
2020-10-21 15:53:46 -0300

    refact: Get data from redis repository

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -21,7 +21,9 @@ export default class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users;
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
